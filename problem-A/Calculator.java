a00c4f78be9646c9d07fda2dbefcd9b79ddd1f67
Yerasyl0405 <124430197+Yerasyl0405@users.noreply.github.com>
2023-02-18 14:17:48 +0600

    Update Calculator.java

--- problem-A/Calculator.java
+++ problem-A/Calculator.java
@@ -1,20 +1,4 @@
-import java.util.Scanner;
-public class Main {    public static void main(String[] args) {
-        Scanner in  = new Scanner(System.in);
-        while (true) {            try {
-                int a = in.nextInt();
-                int b = in.nextInt();
-                Calculator obj1 = new Calculator(a, b);                System.out.println(a + " + " + b + " = " + obj1.Add());
-                Calculator obj2 = new Calculator(a, b);
-                System.out.println(a + " - " + b + " = "  + obj2.Subtract());
-                Calculator obj3 = new Calculator(a, b);                System.out.println(a + " * " + b + " = " + obj3.Multiplication());
-                Calculator obj4 = new Calculator(a, b);
-                System.out.println(a + " / " + b + " = " + obj4.Division());
-            } catch (ArithmeticException e) {                System.out.println("Error: " + e.getMessage());
-            } catch (NumberFormatException e) {
-                System.out.println("Error: Invalid input. Please provide integers only.");            }
-        }    }
-} class Calculator {    private int num1;
+public class Calculator {    private int num1;
     private int num2;    public Calculator (int num1, int num2) {
         this.num1 = num1;        this.num2 = num2;
     }
